fedf1d9f30e51cb74b288965901e7bb565da5c83
Noura <nourabenhssine10@gmail.com>
2026-07-31 16:35:49 +0100

    Update Channel.cpp

--- src/Channel.cpp
+++ src/Channel.cpp
@@ -1,57 +1,181 @@
+
 #include "Channel.hpp"
-#include "Client.hpp"
-#include <sstream>
 
 Channel::Channel(const std::string& name)
-    : _name(name), _userLimit(-1),
-      _inviteOnly(false), _topicRestricted(false)
+    : _name(name), _inviteOnly(false), _topicRestricted(false),
+      _key(""), _userLimit(-1)
 {}
-
 Channel::~Channel() {}
 
-void Channel::addMember(Client* client)
+bool Channel::hasClient(Client* client) const
 {
-    _members[client->getFd()] = client;
-}
-
-void Channel::removeMember(int fd)
-{
-    _members.erase(fd);
-    _operators.erase(fd);
-    _invited.erase(fd);
-}
-
-bool Channel::isMember(int fd) const
-{
-    return _members.count(fd) > 0;
-}
-
-void Channel::addOperator(int fd)   { _operators.insert(fd); }
-void Channel::removeOperator(int fd){ _operators.erase(fd); }
-bool Channel::isOperator(int fd) const { return _operators.count(fd) > 0; }
-
-void Channel::addInvited(int fd)    { _invited.insert(fd); }
-bool Channel::isInvited(int fd) const { return _invited.count(fd) > 0; }
-
-void Channel::broadcast(const std::string& msg, int excludeFd)
-{
-    for (std::map<int, Client*>::iterator it = _members.begin();
-         it != _members.end(); ++it)
+    for (std::vector<Client*>::const_iterator it = _clients.begin();
+         it != _clients.end();
+         ++it)
     {
-        if (it->first != excludeFd)
-            it->second->queueMessage(msg);
+        if (*it == client)
+            return true;
+    }
+    return false;
+}
+
+
+void Channel::addClient(Client* client)
+{
+    if (hasClient(client))
+        return;
+
+    _clients.push_back(client);
+
+    if (_clients.size() == 1)
+        addOperator(client);
+}
+
+void Channel::removeClient(Client* client)
+{
+    for (std::vector<Client*>::iterator it = _clients.begin(); it != _clients.end(); ++it)
+    {
+        if (*it == client)
+        {
+            _clients.erase(it);
+            break;
+        }
+    }
+    removeOperator(client);
+}
+
+bool Channel::isOperator(Client* client) const
+{
+    for (std::vector<Client*>::const_iterator it = _operators.begin(); it != _operators.end(); ++it)
+    {
+        if (*it == client)
+            return true;
+    }
+    return false;
+}
+
+void Channel::addOperator(Client* client)
+{
+    if (!isOperator(client))
+        _operators.push_back(client); 
+}
+
+void Channel::removeOperator(Client* client)
+{
+    for (std::vector<Client*>::iterator it = _operators.begin(); it != _operators.end(); ++it)
+    {
+        if (*it == client)
+        {
+            _operators.erase(it);
+            break;
+        }
     }
 }
 
-std::string Channel::getMemberList() const
+bool Channel::empty() const
 {
-    std::string list;
-    for (std::map<int, Client*>::const_iterator it = _members.begin();
-         it != _members.end(); ++it)
+    return _clients.empty();
+}
+
+const std::string& Channel::getTopic() const
+{
+    return _topic;
+}
+
+void Channel::setTopic(const std::string& topic)
+{
+    _topic = topic;
+}
+void Channel::broadcast(const std::string& msg, Client* sender)
+{
+    for (std::vector<Client*>::iterator it = _clients.begin(); it != _clients.end(); ++it)
     {
-        if (!list.empty()) list += " ";
-        if (_operators.count(it->first)) list += "@";
-        list += it->second->getNickname();
+        if (*it != sender)
+            (*it)->queueMessage(msg);
     }
-    return list;
+}
+
+const std::vector<Client*>& Channel::getClients() const
+{
+    return _clients;
+}
+
+void Channel::addInvited(Client* client)
+{
+    if (!isInvited(client))
+        _invitedClients.push_back(client);
+}
+
+void Channel::removeInvited(Client* client)
+{
+    for (std::vector<Client*>::iterator it = _invitedClients.begin();
+         it != _invitedClients.end(); ++it)
+    {
+        if (*it == client)
+        {
+            _invitedClients.erase(it);
+            break;
+        }
+    }
+}
+
+bool Channel::isInvited(Client* client) const
+{
+    for (std::vector<Client*>::const_iterator it = _invitedClients.begin();
+         it != _invitedClients.end(); ++it)
+    {
+        if (*it == client)
+            return true;
+    }
+    return false;
+}
+
+bool Channel::isInviteOnly() const 
+{
+    return _inviteOnly;
+}
+
+void Channel::setInviteOnly(bool value) 
+{
+    _inviteOnly = value;
+}
+
+bool Channel::isTopicRestricted() const 
+{
+    return _topicRestricted;
+}
+void Channel::setTopicRestricted(bool value)
+{
+    _topicRestricted = value;
+}
+
+bool Channel::hasKey() const
+{
+    return !_key.empty();
+}
+void Channel::setKey(const std::string& key)
+{
+    _key = key;
+}
+bool Channel::checkKey(const std::string& key) const 
+{
+    return _key == key;
+}
+
+int Channel::getUserLimit() const 
+{
+    return _userLimit;
+}
+void Channel::setUserLimit(int limit)
+{
+    _userLimit = limit;
+}
+bool Channel::isFull() const
+{
+    return (_userLimit >= 0 && (int)_clients.size() >= _userLimit);
+}
+
+const std::string& Channel::getName() const 
+{
+    return _name;
 }
